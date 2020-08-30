bfa0578864b4489480837510865fd98a3ab5758b
hrithie <hrithie@hotmail.com>
2020-08-30 13:57:07 -0400

    Added new challenges, bash, and service files

---
+++ start-challenges.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+#NEE
+#command=$(docker-compose up -d)
+cd ./netcatintome
+pwd
+command=$(docker-compose up -d)
+echo "Netcat Into Me is up now!"
+cd ../
+
+
+
+cd ./injectme
+pwd
+command=$(docker-compose up -d)
+echo "Inject Me is up now!"
+cd ../
+
+
+cd ./safensound
+pwd
+command=$(docker-compose up -d)
+echo "Safe n Sound is up now!"
+cd ../
+
+
+cd ./supersecurelogin
+pwd
+command=$(docker-compose up -d)
+echo "Super Secure Login is up now!"
+cd ../
+
+
+cd ./xorhunt
+pwd
+command=$(docker-compose up -d)
+echo "XOR Hunt is up now!"
+cd ../
+
+
+
+#Hrithie
+cd ./canyourespond
+pwd
+command=$(docker-compose up -d)
+echo "Can You Respond is up now!"
+cd ../
+
+
+cd ./mrrobot
+pwd
+command=$(docker-compose up -d)
+echo "Mr Robot is up now!"
+cd ../
+
+
+cd ./siteseeing
+pwd
+command=$(docker-compose up -d)
+echo "Siteseeing is up now!"
+cd ../
+
+
+#Ariel
+cd ./AdminHijinx
+pwd
+command=$(docker-compose up -d)
+echo "Admin Hijinx is up now!"
+cd ../
+
+cd ./FastCash18Up
+pwd
+command=$(docker-compose up -d)
+echo "FastCash18Up is up now!"
+cd ../
+
+cd ./PythonEscape
+pwd
+command=$(docker-compose up -d)
+echo "PythonEscape is up now!"
+cd ../
+
+cd ./SniffSniff
+pwd
+command=$(docker-compose up -d)
+echo "SniffSniff is up now!"
+cd ../
+
+cd ./SocialEngineering101
+pwd
+command=$(docker-compose up -d)
+echo "SocialEngineering101 is up now!"
+cd ../
+
+
+cd ./XSS4Kids
+pwd
+command=$(docker-compose up -d)
+echo "XSS4Kids is up now!"
+cd ../
+
+
+#Gwen
+command=$(cp -r ./FreeFlag /var/www/html)
+echo "Free Flag is up now!"
+
+command=$(cp -r ./WhereIsMyKey /var/www/html)
+echo "Where is my Key is up now!"
+
+
+
+
+
+
+
+
+
+
+
+##LEAVE THIS AT THE BOTTOM! PLACE YOUR CONFIG ON TOP OF THIS PLZZZZZZZZZ ~NEE <3
+
+#Hrithie (FTP)
+#!/bin/bash
+
+command=$(apt-get remove vsftpd -y)
+command=$(apt-get install vsftpd)
+sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd.conf
+command=$(echo 'anon_root=/home/lol' >> /etc/vsftpd.conf)
+cd /
+cd ./home
+command=$(mkdir lol)
+cd ./lol
+command=$(touch flag.txt)
+command=$(echo 'flag{dwarf}' >> flag.txt)
+command=$(systemctl start vsftpd)
+command=$(systemctl enable vsftpd)
+echo "Fly To Pluto is up now!"
+
+
+#Gwen (Nginx)
+#!/bin/bash
+
+apt-get install nginx -y
+sed -i 's/80 default_server;/8080 default_server;/g' /etc/nginx/sites-enabled/default
+sed -i 's/[::]:80 default_server;/[::]:8080 default_server;/g' /etc/nginx/sites-enabled/default
+systemctl restart nginx
+systemctl enable nginx
+echo "Nginx Installed"
+
+
+#CTFD
+cd /root/CTFd
+pwd
+command=$(docker-compose up -d)
+echo "CTFd is up now!"
---
+++ stop-challenges.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+#NEE
+cd ./netcatintome
+pwd
+command=$(docker-compose down)
+echo "Netcat Into Me has now been stopped!"
+cd ../
+
+
+
+cd ./injectme
+pwd
+command=$(docker-compose down)
+echo "Inject Me has now been stopped!"
+cd ../
+
+
+cd ./safensound
+pwd
+command=$(docker-compose down)
+echo "Safe n Sound has now been stopped!"
+cd ../
+
+
+cd ./supersecurelogin
+pwd
+command=$(docker-compose down)
+echo "Super Secure Login has now been stopped!"
+cd ../
+
+
+cd ./xorhunt
+pwd
+command=$(docker-compose down)
+echo "XOR Hunt has now been stopped!"
+cd ../
+
+
+#HRITHIE
+cd ./canyourespond
+pwd
+command=$(docker-compose down)
+echo "Can You Respond has now been stopped!"
+cd ../
+
+
+cd ./mrrobot
+pwd
+command=$(docker-compose down)
+echo "Mr Robot has now been stopped!"
+cd ../
+
+
+cd ./siteseeing
+pwd
+command=$(docker-compose down)
+echo "Siteseeing has now been stopped!"
+cd ../
+
+
+
+#Ariel
+cd ./AdminHijinx
+pwd
+command=$(docker-compose down)
+echo "Admin Hijinx has now been stopped!"
+cd ../
+
+cd ./FastCash18Up
+pwd
+command=$(docker-compose down)
+echo "FastCash18Up has now been stopped!"
+cd ../
+
+cd ./PythonEscape
+pwd
+command=$(docker-compose down)
+echo "PythonEscape is has now been stopped!"
+cd ../
+
+cd ./SniffSniff
+pwd
+command=$(docker-compose down)
+echo "SniffSniff has now been stopped!"
+cd ../
+
+cd ./SocialEngineering101
+pwd
+command=$(docker-compose down)
+echo "SocialEngineering101 has now been stopped!"
+cd ../
+
+
+cd ./XSS4Kids
+pwd
+command=$(docker-compose down)
+echo "XSS4Kids has now been stopped!"
+cd ../
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##LEAVE THIS AT THE BOTTOM! PLACE YOUR CONFIG ON TOP OF THIS PLZZZZZZZZZ ~NEE <3
+
+#Hrithie (FTP)
+command=$(apt-get remove vsftpd -y)
+command=$(rm -rf /home/lol/)
+echo "Fly To Pluto has now been stopped!"
+
+
+#Gwen (Nginx)
+#!/bin/bash
+sudo systemctl disable nginx
+sudo systemctl stop nginx
+sudo apt-get remove nginx -y
+sudo apt-get purge nginx nginx-common -y
+sudo rm -rf /etc/nginx/sites-enabled/default
+echo "Nginx has been stopped!"
+
+command=$(rm -rf /var/www/html/FreeFlag)
+echo "Free Flag has been stopped!"
+
+command=$(rm -rf /var/www/html/WhereIsMyKey)
+echo "Where is my Key has been stopped!"
+
+
+#CTFD
+cd /root/CTFd
+pwd
+command=$(docker-compose down)
+echo "CTFd has now been stopped!"
